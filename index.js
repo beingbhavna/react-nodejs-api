e0618546162a471e66caa88bbbd9090c5fa7539d
Bhavna Sharma <bhavna@i2k2.com>
2025-07-18 17:43:35 +0530

    create endpoint for login!!

--- index.js
+++ index.js
@@ -9,10 +9,26 @@ app.get('/', (req, resp) => {
     resp.send("App is working now");
 });
 
-app.post("/register", async(req, resp) => {
+app.post("/register", async (req, resp) => {
     const user = new User(req.body);
-    const result = await user.save();
+    let result = await user.save();
+    result = result.toObject();
+    delete result.password;
     resp.send(result);
+});
+
+
+app.post("/login", async (req, resp) => {
+    if (req.body.password && req.body.email) {
+        const user = await User.findOne(req.body).select("-password");
+        if (user) {
+            resp.send(user);
+        } else {
+            resp.send({ result: "No User Found" });
+        }
+    } else {
+        resp.send({ result: "No User Found" });
+    }
 });
 
 app.listen(5600);
